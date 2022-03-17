9cf0f18adcc8a7c98c77a6938588772ac9c7ab9e
Aaliyah Paige <aaliyahpaige21@gmail.com>
2022-03-17 11:02:36 -0400

    adding more changes

--- DragonCave.java
+++ DragonCave.java
@@ -5,20 +5,23 @@ public class DragonCave {
         try (Scanner getInput = new Scanner(System.in)) {
           System.out.println("You are in a land full of dragons.");
           System.out.println("In front of you, you see two caves.");
-          System.out.println("In one cave, the dragon is friendly and will share his treasue with you");
+          System.out.println(
+          "In one cave, the dragon is friendly and will share his treasue with you");
           System.out.println("The other dragon is greedy and hungry and will eat you on sight");
           System.out.println("Which cave will you go into? Enter 1 or 2");
-          String input = getInput.nextLine();
+          int input = getInput.nextInt();
           System.out.println("You have decided cave " + input);
         
-         String output = input;
-        if(output.equals("1")){
+         
+        if(input == 1){
             System.out.println("You approach the cave...");
             System.out.println("It is dark and spooky...");
             System.out.println("A large dragon jumps out in front of you!");
             System.out.println("He opens his jaws and...");
             System.out.println("Gobbles you down in one bite!");
         }
+        }catch (Exception e){
+            System.out.println("Please enter a valid number.");
         }
     }
 }
--- NumberGuess.java
+++ NumberGuess.java
@@ -1,44 +1,61 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class NumberGuess {
-    public static void main(String[] args) {
-        
-        int randNum;
-           randNum = (int)(Math.random()*20);
-        
-        try(Scanner user = new Scanner(System.in)) {
-            
-            int guess;
-            int count = 0;
-           
-              
-            System.out.println("Hello! What is your name?");
-            String name = user.nextLine();
-            System.out.println("Well " + name + ", I am thinking of a number between 1 and 20.");
-                do{
-            System.out.println("Take a guess.");
-            guess = user.nextInt(); 
-            count++;
+//import org.junit.Test;
 
-            if(guess == randNum)
-                  System.out.println("Good job " + name + "You guessed my number in " + count + " guesses!");
-                  
-           if (guess < randNum)
-                 System.out.println("Your guess is too low");  
-                
-                
-            if(guess > randNum)
-                System.out.println("Your guess is too high");
-                    
-                
+public class NumberGuess {
+  public static void main(String[] args) {
+      
+      int randNum;
+         randNum = (int)(Math.random()*20);
+      
+      try(Scanner user = new Scanner(System.in)) {
+          
+          int guess;
+          int count = 0;
+         
             
-                }while (guess != randNum);
+          System.out.println("Hello! What is your name?");
+          String name = user.nextLine();
+          System.out.println("Well " + name + ", I am thinking of a number between 1 and 20.");
+              do{
+          System.out.println("Take a guess.");
+          guess = user.nextInt(); 
+          count++;
+
+          if(guess == randNum)
+                System.out.println("Good job " + name + " You guessed my number in " + count + " guesses!");
+                
+         if (guess < randNum)
+               System.out.println("Your guess is too low");  
               
-                 
               
-             
-             
-        }
-       
-    }
+          if(guess > randNum)
+              System.out.println("Your guess is too high");
+                  
+              
+          
+              }while (guess != randNum);      
+           
+           
+      }catch(InputMismatchException e){
+      System.out.println("Please Enter a valid number");}
+            
+     
+  }
 }
+        
+        
+    
+             
+   
+    
+
+      //   @Test
+    //   public TestGuessNum{
+
+     
+    
+
+     
+    
