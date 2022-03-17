9802170def31f06f4c5b56bb27fef2a1196d814f
Aaliyah Paige <aaliyahpaige21@gmail.com>
2022-03-17 14:42:53 -0400

    update

--- NumberGuess.java
+++ NumberGuess.java
@@ -1,24 +1,21 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//import org.junit.Test;
+import org.junit.Test;
 
 public class NumberGuess {
-  public static void main(String[] args) {
-      
-      int randNum;
-         randNum = (int)(Math.random()*20);
-      
-      try(Scanner user = new Scanner(System.in)) {
-          
-          int guess;
-          int count = 0;
-         
-            
-          System.out.println("Hello! What is your name?");
-          String name = user.nextLine();
-          System.out.println("Well " + name + ", I am thinking of a number between 1 and 20.");
-              do{
+
+static Scanner user = new Scanner(System.in);
+   
+static void Game(String name){
+  int randNum =(int)(Math.random()*20);
+  int guess;
+  int count = 0;
+try{
+  do{
           System.out.println("Take a guess.");
           guess = user.nextInt(); 
           count++;
@@ -40,22 +37,36 @@ public class NumberGuess {
            
       }catch(InputMismatchException e){
       System.out.println("Please Enter a valid number");}
-            
-     
-  }
-}
-        
-        
-    
              
-   
-    
+}
 
-      //   @Test
-    //   public TestGuessNum{
+  public static void main(String[] args) {
+       System.out.println("Hello! What is your name?");
+       if(user.hasNext()){
+       String name = user.nextLine();
+      System.out.println("Well " + name + ", I am thinking of a number between 1 and 20.");
+       Game(name);       
 
-     
+       }  
+      }   
     
+               
+
+   @Test
+   public void testCheckName(){
+    assertEquals("", "", "Name given");
+  
+   }
+
+   @Test
+   public void testIntegerEntered(){
+      assertEquals(20 , 20, "A number was entered.");
+   }
+  
+    
+  }   
+
+
 
      
     
